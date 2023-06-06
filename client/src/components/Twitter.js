550803ddc60bc84c02315c770e0d3a1664661995
jason-daiuto <108539044+jason-daiuto@users.noreply.github.com>
2023-06-06 17:13:24 -0400

    twitter auth final

--- client/src/components/Twitter.js
+++ client/src/components/Twitter.js
@@ -2,31 +2,6 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function Twitter() {
-    const [tweets, setTweets] = useState([]);
-    const [tweetText, setTweetText] = useState('');
-  
-    useEffect(() => {
-      fetchTweets();
-    }, []);
-  
-    const fetchTweets = async () => {
-      try {
-        const response = await axios.get('api/v1/tweets');
-        setTweets(response.data.tweets);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-  
-    const authTweet = async () => {
-      try {
-        const response = await axios.get('api/v1/twitter/auth/twitter');
-        console.log(response);
-
-      } catch (error) {
-        console.error(error);
-      }
-    };
 
     const handleTwitterAuth = () => {
         window.location.href = 'https://walrus-app-zynat.ondigitalocean.app/auth/twitter';
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const db = require("./models");
 const passport = require('passport');
+const TwitterStrategy = require('passport-twitter').Strategy;
 const SequelizeSession = require('connect-session-sequelize')(session.Store)
 const store = new SequelizeSession({ db: db.sequelize })
 const usersRouter = require('./api/users');
@@ -43,6 +44,33 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/api/v1/users', usersRouter);
 app.use('/api/v1/twitter', twitterRouter);
+
+// Configure Twitter authentication strategy
+passport.use(new TwitterStrategy({
+  consumerKey: '7tVzrnl36nY4HRuFfgylqbTsw',
+  consumerSecret: 'cFx0ctjvpIxxLwsc5vCbIj3tsAvtacfYkw311VIipqvXmWWTdm',
+  callbackURL: 'https://walrus-app-zynat.ondigitalocean.app/auth/twitter/callback',
+  passReqToCallback: true,
+}, async (req, token, tokenSecret, profile, done) => {
+  try {
+    // Assuming you stored user ID in req.session
+    // @ts-ignore
+    const userId = req.session.userId;
+    // Find the existing user in the database
+    const user = await db.User.findOne({ where: { id: userId } });
+
+    if (user) {
+      // Update the user's Twitter tokens
+      await user.update({ twitter: JSON.stringify({ twitterToken: token, twitterSecret: tokenSecret }) });
+      done(null, user);
+    } else {
+      // Handle the case when the user is not found in the database
+      done(new Error('User not found.'));
+    }
+  } catch (error) {
+    done(error);
+  }
+}));
 
 // Initialize Passport
 app.use(passport.initialize());
