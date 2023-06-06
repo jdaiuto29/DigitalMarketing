c232f3290b039c4ab47af45738837472bd2995bc
jason-daiuto <108539044+jason-daiuto@users.noreply.github.com>
2023-06-06 16:10:01 -0400

    quick fix

--- config/config.js
+++ config/config.js
@@ -3,19 +3,19 @@ module.exports = {
   "development": {
 
     // live deploy config
-    // username: process.env.DB_USER,
-    // password: process.env.DB_PASS,
-    // database: process.env.DB_NAME,
-    // host: process.env.DB_HOST,
-    // port: 25060,
-    // dialect: "postgres",
-    // dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: 25060,
+    dialect: "postgres",
+    dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
 
-    "username": process.env.DB_USER || "postgres",
-    "password": process.env.DB_PASS || "Giants115@",
-    "database": "digital-marketing1",
-    "host": process.env.DB_HOST || "localhost",
-    "dialect": "postgres"
+    // "username": process.env.DB_USER || "postgres",
+    // "password": process.env.DB_PASS || "Giants115@",
+    // "database": "digital-marketing1",
+    // "host": process.env.DB_HOST || "localhost",
+    // "dialect": "postgres"
   },
   "production": {
     "use_env_variable": "DATABASE_URL",
