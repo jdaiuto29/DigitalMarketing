ae86bcd805d970ea13cd7d085912d195abfcc7c0
jason-daiuto <108539044+jason-daiuto@users.noreply.github.com>
2023-06-07 01:24:41 -0400

    tweet test

--- api/twitter.js
+++ api/twitter.js
@@ -1,5 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const models = require('../models'); // Assuming you have a User model defined
+const Twit = require('twit');
+
+// API endpoint to send tweets
+app.post('/tweets', async (req, res) => {
+    const { tweetContent } = req.body;
+    const userId = req.session.user.id
+  
+    try {
+      const user = await models.User.findOne({ where: { userId } });
+      if (!user.twitter) {
+        return res.status(404).json({ error: 'User tokens not found' });
+      }
+  
+      const twitClient = new Twit({
+        consumerKey: '7tVzrnl36nY4HRuFfgylqbTsw',
+        consumerSecret: 'cFx0ctjvpIxxLwsc5vCbIj3tsAvtacfYkw311VIipqvXmWWTdm',
+        access_token: user.twitter.twitterToken,
+        access_token_secret: user.twitter.twitterSecret,
+      });
+  
+      await twitClient.post('statuses/update', { status: tweetContent });
+  
+      res.status(200).json({ message: 'Tweet sent successfully' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error posting tweet' });
+    }
+  });
 
 module.exports = router;
--- client/src/components/Twitter.js
+++ client/src/components/Twitter.js
@@ -2,18 +2,37 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function Twitter() {
+    const [tweetContent, setTweetContent] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('/tweets', {
+                tweetContent,
+                userId: 123, // The user ID associated with the tokens
+            });
+            console.log(response.data); // Tweet sent successfully message
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     const handleTwitterAuth = () => {
         window.location.href = 'https://walrus-app-zynat.ondigitalocean.app/auth/twitter';
-      };      
-    
-  
-    return (
-      <>
-                    <button onClick={handleTwitterAuth}>Authorize with Twitter</button>
+    };
 
-              </>
+
+    return (
+        <>
+            <button onClick={handleTwitterAuth}>Authorize with Twitter</button>
+
+            <form onSubmit={handleSubmit}>
+                <textarea value={tweetContent} onChange={(e) => setTweetContent(e.target.value)} />
+                <button type="submit">Tweet</button>
+            </form>
+
+        </>
     );
-  }
-  
-  export default Twitter;
+}
+
+export default Twitter;
