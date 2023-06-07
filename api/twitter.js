f83f61390fa549ff58a329506e527f7d2858ae63
jason-daiuto <108539044+jason-daiuto@users.noreply.github.com>
2023-06-07 01:44:19 -0400

    tweet test

--- api/twitter.js
+++ api/twitter.js
@@ -5,29 +5,29 @@ const Twit = require('twit');
 
 // API endpoint to send tweets
 router.post('/tweets', async (req, res) => {
-    const { tweetContent } = req.body;
-    const userId = req.session.user.id
-  
-    try {
-      const user = await models.User.findOne({ where: { userId } });
-      if (!user.twitter) {
-        return res.status(404).json({ error: 'User tokens not found' });
-      }
-  
-      const twitClient = new Twit({
+  const { tweetContent } = req.body;
+  const userId = req.session.user.id;
+
+  try {
+    const user = await models.User.findOne({ where: { id: userId } });
+    if (!user || !user.twitter) {
+      return res.status(404).json({ error: 'User tokens not found' });
+    }
+
+    const twitClient = new Twit({
         consumerKey: '7tVzrnl36nY4HRuFfgylqbTsw',
         consumerSecret: 'cFx0ctjvpIxxLwsc5vCbIj3tsAvtacfYkw311VIipqvXmWWTdm',
-        access_token: user.twitter.twitterToken,
-        access_token_secret: user.twitter.twitterSecret,
-      });
-  
-      await twitClient.post('statuses/update', { status: tweetContent });
-  
-      res.status(200).json({ message: 'Tweet sent successfully' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Error posting tweet' });
-    }
-  });
+      access_token: user.twitter.twitterToken,
+      access_token_secret: user.twitter.twitterSecret,
+    });
+
+    await twitClient.post('statuses/update', { status: tweetContent });
+
+    res.status(200).json({ message: 'Tweet sent successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error posting tweet' });
+  }
+});
 
 module.exports = router;
--- client/src/components/Twitter.js
+++ client/src/components/Twitter.js
@@ -1,38 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
-
 function Twitter() {
-    const [tweetContent, setTweetContent] = useState('');
+  const [tweetContent, setTweetContent] = useState('');
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post('/api/v1/twitter/tweets', {
-                tweetContent,
-            });
-            console.log(response.data); // Tweet sent successfully message
-        } catch (error) {
-            console.error(error);
-        }
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('/api/v1/twitter/tweets', {
+        tweetContent,
+      });
+      console.log(response.data); // Tweet sent successfully message
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    const handleTwitterAuth = () => {
-        window.location.href = 'https://walrus-app-zynat.ondigitalocean.app/auth/twitter';
-    };
+  const handleTwitterAuth = () => {
+    window.location.href = 'https://walrus-app-zynat.ondigitalocean.app/auth/twitter';
+  };
 
+  return (
+    <>
+      <button onClick={handleTwitterAuth}>Authorize with Twitter</button>
 
-    return (
-        <>
-            <button onClick={handleTwitterAuth}>Authorize with Twitter</button>
-
-            <form onSubmit={handleSubmit}>
-                <textarea value={tweetContent} onChange={(e) => setTweetContent(e.target.value)} />
-                <button type="submit">Tweet</button>
-            </form>
-
-        </>
-    );
+      <form onSubmit={handleSubmit}>
+        <textarea value={tweetContent} onChange={(e) => setTweetContent(e.target.value)} />
+        <button type="submit">Tweet</button>
+      </form>
+    </>
+  );
 }
 
 export default Twitter;
